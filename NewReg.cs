2848e9243b0d59f099b3b118e7ef5e54092560b5
Elizabeth-L-Mawphlang <72000872+Elizabeth-L-Mawphlang@users.noreply.github.com>
2023-04-09 22:09:16 +0530

    Add files via upload

---
+++ NewReg.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Personal.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace Personal.Controllers
+{
+    public class NewReg : Controller
+    {
+        // GET: NewReg
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(userclass uc,HttpPostedFileBase file)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+            string sqlquery = "insert into tblRegistration (Img,rname,lname,email,gender,address,city,state,rcode,phone) values (@Img,@rname,@lname,@email,@gender,@address,@city,@state,@rcode,@phone)";
+            SqlCommand sqlcomm = new SqlCommand(sqlquery, sqlconn);
+            sqlconn.Open();
+            if (file != null && file.ContentLength > 0)
+            {
+                string filename = Path.GetFileName(file.FileName);
+                string imgpath = Path.Combine(Server.MapPath("~/Images/"),filename);
+                file.SaveAs(imgpath);
+            }
+            sqlcomm.Parameters.AddWithValue("@Img", "~/Images/"+file.FileName);
+            sqlcomm.Parameters.AddWithValue("@rname", uc.rname);
+            sqlcomm.Parameters.AddWithValue("@lname", uc.lname);
+            sqlcomm.Parameters.AddWithValue("@email", uc.email);
+            sqlcomm.Parameters.AddWithValue("@gender", uc.gender);
+            sqlcomm.Parameters.AddWithValue("@address", uc.address);
+            sqlcomm.Parameters.AddWithValue("@city", uc.city);
+            sqlcomm.Parameters.AddWithValue("@state", uc.state);
+            sqlcomm.Parameters.AddWithValue("@rcode", uc.rcode);
+            sqlcomm.Parameters.AddWithValue("@phone", uc.phone);
+            sqlcomm.ExecuteNonQuery();
+            sqlconn.Close();
+            ViewData["Message" ]= "User Record" + uc.rname + "Is Saved Successfully!";
+            return View();
+        }
+    }
+}
---
+++ userclass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using System.Runtime.Remoting.Services;
+
+namespace Personal.Models
+{
+    public class userclass
+    {
+        [Required(ErrorMessage="Upload Profile Image!")]
+        [Display(Name ="Profile Image:")]
+        public string Img { get; set; }
+        [Required(ErrorMessage = "Enter Username!")]
+        [Display(Name = "Enter Username:")]
+        public string rname { get; set; }
+        [Required(ErrorMessage = "Enter Lastname!")]
+        [Display(Name = "Enter Lastname:")]
+        public string lname { get; set; }
+        [Required(ErrorMessage = "Please enter the email address!")]
+        [Display(Name = "Email Id:")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Select Gender!")]
+        [Display(Name = "Gender:")]
+        public string gender { get; set; }
+        [Required(ErrorMessage = "Please enter Address!")]
+        [Display(Name = "Enter Address:")]
+        public string address { get; set; }
+        [Required(ErrorMessage = "Please enter City!")]
+        [Display(Name = "Enter City:")]
+        public string city { get; set; }
+        [Required(ErrorMessage = "Please enter State!")]
+        [Display(Name = "Enter State:")]
+        public string state { get; set; }
+        [Required(ErrorMessage = "Please enter Pincode!")]
+        [Display(Name = "Pincode:")]
+        public int rcode { get; set; }
+        [Required(ErrorMessage = "Please enter Contact Number!")]
+        [Display(Name = "Contact number:")]
+        public string phone { get; set; }
+
+
+    }
+}
